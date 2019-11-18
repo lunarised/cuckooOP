1d8f7eb29467f97b561c88bed186496bc1ab9753
lunarised <j@p7.co.nz>
2019-11-18 19:55:13 +1300

    addedBugReport

--- cuckooNetScript.sh
+++ cuckooNetScript.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-VBoxManage hostonlyif create
+VBoxManage hostonlyif create 
 ip link set vboxnet0 up
 #ip addr add 192.168.56.1/24 dev vboxnet0
+
+
+
+##BUG WITH SCRIPT,
+CREATED ENDLESS ODD NUMBERED vboxnet INSTANCES
